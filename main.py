8688c89795c33a4c52cc92ee2e2976cea66acb51
WildTree3000 <moskwadawid@gmail.com>
2024-07-15 22:33:44 +0100

    finished project

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ def main():                                                     # main code brac
     num_words = get_num_words(text)                             # return the number of words in the book
     character_count = get_char_count(text)                      
     print(f"{num_words} words found in the document")           # print our the message
+    print (f"Below is a list of how many times each character appeared in the book{character_count}")         
 
 def get_num_words(text):                                        # code for word cound in the book
     words = text.split()                                        # spliting the book in to separate words
@@ -14,10 +15,16 @@ def get_book_text(book_path):                                   # code for readi
         return f.read()                                         #
     
 def get_char_count(text):
-    lowercase_char = text.lower()
     import collections
-    char_count = collections.Counter (lowercase_char)
+    lowercase_text = text.lower()
+    char_count = collections.Counter(lowercase_text)
     sorted_char_count = dict(sorted(char_count.items()))
-    print (sorted_char_count)
+    finished_character_count = {}
+
+    for key in sorted_char_count:
+        if key.isalpha():
+            finished_character_count[key] = sorted_char_count[key] 
+
+    return (finished_character_count)
 
 main()
